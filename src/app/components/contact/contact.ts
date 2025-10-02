31a6904f493ed824c19d123565e1cb6f110c3772
Stephans-1 <kukustephansneriskwei@gmail.com>
2025-10-02 11:29:13 +0000

    links

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.html',
   styles: ``
 })
 export class Contact {
-
+  whatsapp_icon = faWhatsapp;
 }
---
+++ src/app/components/shared/icons/icons.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import * as simpleIcons from 'simple-icons/icons';
+
+@Component({
+  selector: 'app-icon',
+  imports: [],
+  templateUrl: './icons.html',
+  styles: ``
+})
+export class IconsComponent {
+}
